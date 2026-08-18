17dad42ad24e4496c53fd4532922c10010e73591
jayrana2804 <ranaparesh1754@gmail.com>
2026-08-18 10:01:46 +0530

    Add files via upload

---
+++ p5.3(1).cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,i,j;
+    for(i=1;i<=n;i=i+1)
+    {
+        for(j=1;j<=i;j=j+1)
+        {
+            cout<<"n";
+        }
+        cout<<endl;
+    }
+return 0;
+}
+
